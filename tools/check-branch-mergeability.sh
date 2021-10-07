5cf59359289fdaeba37df98704103621197b8efc
EmilySeville7cfg <EmilySeville7cf@gmail.com>
2021-10-07 21:49:30 +1000

    Tool script updated: all branch cases handled

--- tools/check-branch-mergeability.sh
+++ tools/check-branch-mergeability.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
-message=
-declare -i status=0
+declare -ir SUCCESS=0
+declare -ir WRONG_BRANCH=1
 
-if [[ $GITHUB_HEAD_REF == @(feature|bugfix)/* ]]; then
-    message='Source branch is correct.'
-else
-    message="Source ($GITHUB_HEAD_REF) branch is incorrect: it must be feature or bugfix branch."
-    status+=1
-fi
+case "$GITHUB_BASE_REF" in
+    dev)
+        if [[ $GITHUB_HEAD_REF != @(feature|bugfix)/* ]]; then
+            echo "Source branch must be \"feature/<issue-id>\" or \"bugfix/<issue-id>\" branch \
+when merged to \"dev\", but now it is \"$GITHUB_HEAD_REF\"" >&2
+            exit $WRONG_BRANCH
+        fi
+    ;;
+    master)
+        if [[ $GITHUB_HEAD_REF != dev ]]; then
+            echo "Source branch must be \"dev\" branch \
+when merged to \"master\", but now it is \"$GITHUB_HEAD_REF\"" >&2
+            exit $WRONG_BRANCH
+        fi
+    ;;
+    *)
+        echo "Base branch must be \"dev\" or \"master\" branch, but now it is \"$GITHUB_BASE_REF\"." >&2
+        exit $WRONG_BRANCH
+    ;;
+esac
 
-if [[ $GITHUB_BASE_REF == "dev" ]]; then
-    message+="\nBase branch is correct."
-else
-    message+="\nBase ($GITHUB_BASE_REF) branch is incorrect: it must be dev branch."
-    status+=1
-fi
-
-echo -e "$message"
-(( status == 0 ))
-exit $?
+exit $SUCCESS
